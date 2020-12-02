e321173c7c744f75a03a144a5f54d1946149b1a2
aperinevg <aperinevg@yandex.ru>
2020-12-02 22:50:08 +0300

    first commit

---
+++ src/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  style  from './MainPage.module.css'
+
+
+function MainPage() {
+    return (
+      <div className={style.main}>
+        
+      </div>
+    );
+}
+
+export default MainPage; 
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style  from './Navbar.module.css'
+
+
+function Navbar() {
+    return (
+      <div className={style.navbar}>
+        
+      </div>
+    );
+}
+
+export default Navbar; 
